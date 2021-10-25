885469cd946626fb1fb90311bb812a6c19cb5ff1
SungU Kang <20sunrin129@sunrint.hs.kr>
2021-10-25 18:29:54 +0900

    getter / setter

--- src/core/World/World.ts
+++ src/core/World/World.ts
@@ -13,4 +13,31 @@ export class World {
         this._characters = [];
         this._physics = new Physics(this._map, this._characters);
     }
+
+    
+    getMap() {
+        return this._map;
+    }
+
+    setMap(map: WorldMap) {
+        this._map = map;
+    }
+
+
+    getCharacters() {
+        return this._characters;
+    }
+
+    setCharacters(characters: Character<any>[]) {
+        this._characters = characters;
+    }
+
+
+    getPhysics() {
+        return this._physics;
+    }
+
+    setPhysics(physics: Physics) {
+        this._physics = physics;
+    }
 }
