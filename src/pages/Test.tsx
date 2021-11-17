8316c34d078b4ef437a30dafb15580a175b265cb
Lunuy <luminuy@gmail.com>
2021-11-17 13:56:50 +0900

    Add assets & physics test

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -7,41 +7,113 @@ import { Floor } from "../core/Map/Objects/Floor";
 import { Wall } from "../core/Map/Objects/Wall";
 import { WorldMap } from "../core/Map/WorldMap";
 import { Renderer } from "../core/Renderer/Renderer";
+import { Direction } from "../core/types/Base";
 import { ImageShape } from "../core/types/Shape/ImageShape";
 import { World } from "../core/World/World";
 import { Controler } from "../game/Controller/Controller";
+import { physicsLineFactory } from "../game/physicsLine/physicsLineFactory"
 
 
 const worldMap = new WorldMap({height: 50, width: 50});
 const world = new World({height: 100, width: 100});
 
-const floor = new Floor(
-    new ImageShape({
-        width: 1,
-        height: 1,
-    }, '/assets/tile/tile000.png'),
-);
-worldMap.getFloors().push(floor);
-
-const wall = new Wall(
-    new ImageShape({
-        width: 2,
-        height: 1,
-    }, 'https://cdn.pixabay.com/photo/2019/11/27/03/16/white-4655992_960_720.jpg'),
-);
-wall.setPosition({x: 1, y: 1});
-worldMap.getWalls().push(wall);
 
 
-const effect = new Effect(
-    new ImageShape({
-        width: 2,
-        height: 2,
-    }, 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Sky.jpg/1280px-Sky.jpg')
-);
-effect.setPosition({x: 4, y: 4});
-worldMap.getEffects().push(effect);
+// Floor
 
+enum FloorTile {
+    GRASS = "014",
+    RIGHT_BOTTOM_GRASS = "000",
+    RIGHT_TOP_GRASS = "026",
+    LEFT_BOTTOM_GRASS = "002",
+    LEFT_TOP_GRASS = "028",
+    BOTTOM_GRASS = "021",
+    RIGHT_GRASS = "013",
+    LEFT_GRASS = "015",
+    TOP_GRASS = "027"
+}
+
+{
+    const floorMap: FloorTile[][] = [
+        [FloorTile.RIGHT_BOTTOM_GRASS, ...new Array(8).fill(FloorTile.BOTTOM_GRASS), FloorTile.LEFT_BOTTOM_GRASS],
+        ...new Array(8).fill([FloorTile.RIGHT_GRASS, ...new Array(8).fill(FloorTile.GRASS), FloorTile.LEFT_GRASS]),
+        [FloorTile.RIGHT_TOP_GRASS, ...new Array(8).fill(FloorTile.TOP_GRASS), FloorTile.LEFT_TOP_GRASS],
+    ]
+
+    const floors = worldMap.getFloors();
+
+    for(let y = 0; y < floorMap.length; y++) {
+        const line = floorMap[y];
+        for(let x = 0; x < line.length; x++) {
+            const tileId = line[x];
+
+            const floor = new Floor(
+                new ImageShape({
+                    width: 1,
+                    height: 1,
+                }, `/assets/tile/tile${tileId}.png`),
+            );
+
+            floor.setPosition({ x, y });
+
+            floors.push(floor);
+        }
+    }
+}
+
+
+
+
+// Wall
+{
+    {
+        const wall = new Wall(
+            new ImageShape({
+                width: 2,
+                height: 2,
+            }, '/assets/wall/tree.png'),
+        );
+        wall.setPosition({x: 1, y: 1});
+        worldMap.getWalls().push(wall);
+    }
+
+    {
+        const wall = new Wall(
+            new ImageShape({
+                width: 2,
+                height: 2,
+            }, '/assets/wall/tree.png'),
+        );
+        wall.setPosition({x: 6, y: 4});
+        worldMap.getWalls().push(wall);
+    }
+}
+
+
+// Effect
+{
+    const effect = new Effect(
+        new ImageShape({
+            width: 2,
+            height: 2,
+        }, '/assets/effect/snow.gif')
+    );
+    effect.setPosition({x: 4, y: 4});
+    worldMap.getEffects().push(effect);
+}
+
+
+
+// Physics
+{
+    const physicsLineMap = physicsLineFactory(10, 10, [
+        { x: 1, y: 4, direction: Direction.up },
+        { x: 2, y: 4, direction: Direction.up },
+        { x: 6, y: 7, direction: Direction.up },
+        { x: 7, y: 7, direction: Direction.up }
+    ]);
+    worldMap.setPhysicsLineMap(physicsLineMap);
+}
 
 const state: State<1> = new State({
     async action() {
