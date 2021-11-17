87334785a0973f1b527f74461d8fce475efa89c5
SungU Kang <20sunrin129@sunrint.hs.kr>
2021-11-17 13:57:07 +0900

    wall setpos

--- src/core/Map/Objects/Wall.ts
+++ src/core/Map/Objects/Wall.ts
@@ -1,7 +1,12 @@
+import { Point } from "../../types/Base";
 import { GameObject } from "../../types/GameObject";
 import { DomShape } from "../../types/Shape/DomShape";
 import { ImageShape } from "../../types/Shape/ImageShape";
 
 export class Wall extends GameObject {
-
+    
+    setPosition(pos: Point) {
+        pos.y -= this.getShape().getSize().height - 1
+        super.setPosition(pos);
+    }
 }
