744b94e179e3459ebe273113e5fac6c033d3dee1
SungU Kang <20sunrin129@sunrint.hs.kr>
2021-11-17 14:07:50 +0900

    rollback

--- src/core/Map/Objects/Wall.ts
+++ src/core/Map/Objects/Wall.ts
@@ -4,9 +4,4 @@ import { DomShape } from "../../types/Shape/DomShape";
 import { ImageShape } from "../../types/Shape/ImageShape";
 
 export class Wall extends GameObject {
-    
-    setPosition(pos: Point) {
-        pos.y -= this.getShape().getSize().height - 1
-        super.setPosition(pos);
-    }
 }
