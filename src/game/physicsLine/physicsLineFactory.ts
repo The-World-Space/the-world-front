93c417263bd6860c1304a8b9b0197d3221891a4e
SungU Kang <20sunrin129@sunrint.hs.kr>
2021-11-17 13:42:14 +0900

    testcode

---
+++ src/game/physicsLine/physicsLineFactory.ts
@@ -0,0 +1,30 @@
+import { Direction } from "../../core/types/Base";
+
+
+interface physicsLine {
+    x: number;
+    y: number;
+    direction: Direction;
+}
+
+export function physicsLineFactory(height: number, width: number, physicsLines: physicsLine[]) {
+    let res = new Array(height).fill(0)
+                 .map(() => new Array(width).fill(false));
+
+    function setTrue(x: number, y: number, direction: Direction) {
+        const list_dy = [NaN, 0, 1, 0, 0];
+        const list_dx = [NaN, 0, 0, -1, 1];
+
+        const dy = list_dy[direction];
+        const dx = list_dx[direction];
+
+        res[y + dy][2 * x + dx + 1] = true;
+    }
+    
+
+    physicsLines.forEach(({ x, y, direction }) => {
+        setTrue(x, y, direction);
+    });
+
+    return res;
+}
