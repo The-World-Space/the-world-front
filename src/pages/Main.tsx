c71c5e53dabfbc89be5b7a6c7a759146d4ad8388
noname <48761044+noname0310@users.noreply.github.com>
2021-11-18 02:42:04 +0900

    wip

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -5,9 +5,9 @@ import {
 } from 'react-router-dom';
 import { useContext } from "react";
 import styled from "styled-components";
+import twLogo1 from '../components/atoms/tw logo 1.svg';
 import twLogo2 from '../components/atoms/tw logo 2.svg';
 import { MENU_BUTTON_FONT_FAMILY, MENU_BUTTON_FONT_SIZE, MENU_BUTTON_FONT_STYLE, MENU_BUTTON_FONT_WEIGHT } from "./GlobalEnviroment";
-
 
 
 const TopDiv = styled.div`
@@ -16,12 +16,8 @@ const TopDiv = styled.div`
     align-items: center;
     justify-content: space-between;
     width: 100%;
-    height: 100%;
     box-sizing: border-box;
-    padding-right: 10%;
-    padding-left: 10%;
-    padding-top: 2%;
-    padding-bottom: 2%;
+    padding: 2% 10% 2% 10%;
     background: #D7CCC8;
     box-shadow: 5px 5px 20px rgba(0, 0, 0, 0.12);
 `;
@@ -31,25 +27,42 @@ const RowButtonDiv = styled.div`
     flex-direction: row;
     align-items: center;
     justify-content: center;
-    height: 100%;
 `;
 
 const MainButton = styled.button`
-    padding: 10px;
-    border: none;
-    background: #A69B97;
     border-radius: 22.5px;
     margin-right: 10px;
     margin-left: 10px;
+    padding: 8% 12% 8% 12%;
     font-family: ${MENU_BUTTON_FONT_FAMILY};
     font-style: ${MENU_BUTTON_FONT_STYLE};
     font-weight: ${MENU_BUTTON_FONT_WEIGHT};
     font-size: ${MENU_BUTTON_FONT_SIZE};
+    border: none;
+    background: #A69B97;
     color: #FFFFFB;
     &:hover {
         background-color: #857b78;
     }
 `;
+
+const ContentDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+    width: 100%;
+    height: 100%;
+    box-sizing: border-box;
+    background: #FFFFFB;
+`;
+
+const ContentDetailDiv = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+`;
+
 
 
 function Main() {
@@ -64,36 +77,33 @@ function Main() {
                 </Link>
                 <RowButtonDiv>
                     { logged 
-                        ? <Link to="/logout">Logout</Link>
+                        ? <MainButton onClick={() => history.push('/logout')}>Logout</MainButton>
                         : <>
                             <MainButton onClick={() => history.push('/login')}>Login</MainButton>
                             <MainButton onClick={() => history.push('/register')}>Register</MainButton>
                         </>}
                 </RowButtonDiv>
             </TopDiv>
-                
-            <p>
-                <Link to="/login">Login</Link>
-            </p>
-            <p>
-                <Link to="/register">Register</Link>
-            </p>
-            {logged &&
-                <>
-                    <p>
-                        <Link to="/logout">Logout</Link>
-                    </p>
-                    <p>
-                        <Link to="/todo">Todo</Link>
-                    </p>
-                </>
-            }
-            <p>
-                <Link to="/thelab">TheLab</Link>
-            </p>
-            <p>
-                <Link to="/world/0">world/0</Link>
-            </p>
+            <ContentDiv>
+                <div>
+                    <img src={twLogo1} />
+                </div>
+                <ContentDetailDiv>
+                    사용자 지정 기능 확장이 가능한 2D 메타버스 서비스입니다. 탑다운 뷰로, 사용자는 원하는 사용자 지정 서비스를 바닥이나 건물로서 배치할 수 있습니다. 사용자 지정 서비스를 만드는 경우, 바닥이나 건물과의 상호작용을 원하는 대로 처리할 수 있습니다.
+                    <div style={{
+                        borderLeft: '1px solid #000000C0',
+                        height: '100px',
+                        margin: '0% 2% 0% 2%',
+                    }}/>
+                    <img src={`${process.env.PUBLIC_URL}/assets/takahiro.jpg`} />
+                </ContentDetailDiv>
+                <p>
+                    <Link to="/thelab">TheLab</Link>
+                </p>
+                <p>
+                    <Link to="/world/0">world/0</Link>
+                </p>
+            </ContentDiv>
         </>
     );
 }
