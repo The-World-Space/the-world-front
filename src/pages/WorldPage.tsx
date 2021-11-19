38d8f4d98081a43f40926a115617713896c1b645
noname <48761044+noname0310@users.noreply.github.com>
2021-11-19 22:07:38 +0900

    Update WorldPage.tsx

--- src/pages/WorldPage.tsx
+++ src/pages/WorldPage.tsx
@@ -131,7 +131,15 @@ async function makeTestWorld(world?: World) {
         const physicsLineMap = physicsLineFactory(height, height, [
             ...physicsLineList,
         ]);
-        worldMap.setPhysicsLineMap(physicsLineMap);
+        const physmap = worldMap.getPhysicsLineMap();
+        for (let y = 0; y < height; y++) {
+            for (let x = 0; x < width; x++) {
+                const line = physicsLineMap[y][x];
+                if (line) {
+                    physmap[y][x] = line;
+                }
+            }
+        }
         // </temp code>
     }
 
