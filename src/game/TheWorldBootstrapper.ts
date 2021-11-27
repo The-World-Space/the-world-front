dd41644fedbf58af58047105dd4b13859f51c1cf
noname <48761044+noname0310@users.noreply.github.com>
2021-11-27 13:48:44 +0900

    Update TheWorldBootstrapper.ts

--- src/game/TheWorldBootstrapper.ts
+++ src/game/TheWorldBootstrapper.ts
@@ -2,6 +2,7 @@ import { Vector3 } from "three";
 import { CssSpriteAtlasRenderer } from "./component/render/CssSpriteAtlasRenderer";
 import { CssSpriteRenderer } from "./component/render/CssSpriteRenderer";
 import { SpriteAnimator } from "./component/render/SpriteAnimator";
+import { SpriteAtlasAnimator } from "./component/render/SpriteAtlasAnimator";
 import { ZaxisSorter } from "./component/render/ZaxisSorter";
 import { TestExectuer } from "./component/TestExectuer";
 import { IBootstrapper } from "./engine/bootstrap/IBootstrapper";
@@ -12,17 +13,29 @@ export class TheWorldBootstrapper implements IBootstrapper {
     public run(scene: THREE.Scene, gameManager: GameManager): SceneBuilder {
         const instantlater = gameManager.instantlater;
         const camera = gameManager.camera;
-        
+
         camera.position.set(0, 0, 50);
 
-        let charactorAnimator: SpriteAnimator|null = null;
+        let charactorAnimator: SpriteAtlasAnimator|null = null;
+        let tileAnimator: SpriteAnimator|null = null;
 
         return new SceneBuilder(scene)
             .withChild(instantlater.buildGameObject("obj1")
                 .withChild(instantlater.buildGameObject("obj1.1", new Vector3(0, 32, 0))
                     .withComponent(CssSpriteAtlasRenderer, c => {
-                        c.setImage(`${process.env.PUBLIC_URL}/assets/Hyeonjong.png`, 4, 4);
-                        c.imageIndex = 5;
+                        c.setImage(`${process.env.PUBLIC_URL}/assets/charactor/Hyeonjong.png`, 4, 4);
+                    })
+                    .withComponent(SpriteAtlasAnimator, c => {
+                        c.addAnimation("down_idle", [0]);
+                        c.addAnimation("right_idle", [4]);
+                        c.addAnimation("up_idle", [8]);
+                        c.addAnimation("left_idle", [12]);
+                        c.addAnimation("down_walk", [0, 1, 2, 3]);
+                        c.addAnimation("right_walk", [4, 5, 6, 7]);
+                        c.addAnimation("up_walk", [8, 9, 10, 11]);
+                        c.addAnimation("left_walk", [12, 13, 14, 15]);
+                        c.frameDuration = 0.2;
+                        charactorAnimator = c;
                     })))
             .withChild(instantlater.buildGameObject("obj2", new Vector3(0, 0, 0))
                 .withComponent(CssSpriteRenderer)
@@ -55,13 +68,14 @@ export class TheWorldBootstrapper implements IBootstrapper {
                         `${process.env.PUBLIC_URL}/assets/tileRoom3/18.png`,
                     ]);
                     c.frameDuration = 0.1;
-                    charactorAnimator = c;
+                    tileAnimator = c;
                 })
                 .withComponent(ZaxisSorter))
             .withChild(instantlater.buildGameObject("core")
                 .withComponent(TestExectuer, c => {
                     c.setTestFunc(() => {
-                        charactorAnimator!.playAnimation("test_anim1");
+                        charactorAnimator!.playAnimation("down_walk");
+                        tileAnimator!.playAnimation("test_anim1");
                     })
                 }));
     }
