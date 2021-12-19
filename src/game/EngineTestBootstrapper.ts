0d0e04e90943ca163255d3d24b3dd423e06df126
noname <48761044+noname0310@users.noreply.github.com>
2021-12-19 10:05:29 +0900

    Update EngineTestBootstrapper.ts

--- src/game/EngineTestBootstrapper.ts
+++ src/game/EngineTestBootstrapper.ts
@@ -1,5 +1,6 @@
+import { CssCollideTilemapChunkRenderer } from "./component/physics/CssCollideTilemapChunkRenderer";
 import { Camera } from "./component/render/Camera";
-import { TestComponent } from "./component/TestComponent";
+import { TileAtlasItem } from "./component/render/CssTilemapRenderer";
 import { Bootstrapper } from "./engine/bootstrap/Bootstrapper";
 import { SceneBuilder } from "./engine/bootstrap/SceneBuilder";
 export class EngineTestBootstrapper extends Bootstrapper {
@@ -10,6 +11,18 @@ export class EngineTestBootstrapper extends Bootstrapper {
             .withChild(instantlater.buildGameObject("Camera")
                 .withComponent(Camera))
             .withChild(instantlater.buildGameObject("TestObject")
-                .withComponent(TestComponent, c => c.messageHead = "TestComponent"));
+                .withComponent(CssCollideTilemapChunkRenderer, c => {
+                    const tilemap3 = new Image();
+                    tilemap3.src = "/assets/tilemap/room_sanscorridor.png";
+
+                    c.imageSources = [ new TileAtlasItem(tilemap3, 3, 20) ];
+                    c.pointerEvents = false;
+
+                    tilemap3.onload = () => {
+                        tilemap3.onload = null;
+                        debugger;
+                        c.drawTileFromTwoDimensionalArray([[{i: 0, a: 0}]], 0, 0);
+                    }
+                }));
     }
 }
