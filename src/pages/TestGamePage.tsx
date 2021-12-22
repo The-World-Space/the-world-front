9eacae5fa7c29c1f7be050dafd70a49c64565b0c
noname <48761044+noname0310@users.noreply.github.com>
2021-12-22 18:36:02 +0900

    Update TestGamePage.tsx

--- src/pages/TestGamePage.tsx
+++ src/pages/TestGamePage.tsx
@@ -1,28 +1,26 @@
-import { useCallback, useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { TestBootstrapper } from '../game/TestBootstrapper';
 import { Game } from '../game/engine/Game';
 
 function TestGamePage() {
-    let game = useRef<Game>(null);
-    let div = useRef<HTMLDivElement>(null);
+    const div = useRef<HTMLDivElement>(null);
 
-    const onWindowResize = useCallback(() => {
-        if (div.current) game.current?.resizeFramebuffer(div.current.offsetWidth, div.current.offsetHeight);
-    }, [div, game]);
-
-    useEffect( () => { //on mount component
-        window.addEventListener("resize", onWindowResize);
-
+    useEffect(() => { //on mount component
         if (!div.current) throw new Error("div is null");
-        game = new Game(div.current, div.current.offsetWidth, div.current.offsetHeight);
+        const game = new Game(div.current!, div.current!.offsetWidth, div.current!.offsetHeight);
         game.run(TestBootstrapper);
         game.inputHandler.startHandleEvents();
-    }, [onWindowResize]);
 
-    useEffect( () => () => { //on unmount component
-        window.removeEventListener("resize", onWindowResize);
-        game?.dispose();
-    }, [onWindowResize, game]);
+        function onWindowResize() {
+            if (div.current) game?.resizeFramebuffer(div.current.offsetWidth, div.current.offsetHeight);
+        }
+
+        window.addEventListener("resize", onWindowResize);
+        return () => { //on unmount component
+            window.removeEventListener("resize", onWindowResize);
+            game?.dispose();
+        };
+    }, []);
 
     return (<div style = {{height: "100%", width: "100%"}} ref={div}/>);
 }
