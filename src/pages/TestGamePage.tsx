52c39687572e189ec8e13f2799bddb41d25bf424
noname <48761044+noname0310@users.noreply.github.com>
2021-12-22 17:51:50 +0900

    Update TestGamePage.tsx

--- src/pages/TestGamePage.tsx
+++ src/pages/TestGamePage.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 import { TestBootstrapper } from '../game/TestBootstrapper';
 import { Game } from '../game/engine/Game';
 
@@ -6,23 +6,23 @@ function TestGamePage() {
     let game: Game | null = null;
     let div: HTMLDivElement | null = null;
 
+    const onWindowResize = useCallback(() => {
+        if (div) game?.resizeFramebuffer(div.offsetWidth, div.offsetHeight);
+    }, [div, game]);
+
     useEffect( () => { //on mount component
         window.addEventListener('resize', onWindowResize);
-    });
+    }, [onWindowResize]);
+
     useEffect( () => () => { //on unmount component
         window.removeEventListener('resize', onWindowResize);
         game?.dispose();
-    });
-
-    function onWindowResize() {
-        if (div) game?.resizeFramebuffer(div.offsetWidth, div.offsetHeight);
-    }
+    }, [onWindowResize, game]);
 
     return (
         <div style = {{height: '100%', width: '100%'}} ref={ref => {
             div = ref;
             if (ref !== null) {
-                
                 game = new Game(ref, ref.offsetWidth, ref.offsetHeight);
                 game.run(TestBootstrapper);
                 game.inputHandler.startHandleEvents();
