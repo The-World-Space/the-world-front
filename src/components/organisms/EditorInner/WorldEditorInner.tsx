b66f1822d877240e47082adca16e943ac0f412b6
SungU Kang <20sunrin129@sunrint.hs.kr>
2021-12-24 22:02:11 +0900

    WorldEditor

--- src/components/organisms/EditorInner/WorldEditorInner.tsx
+++ src/components/organisms/EditorInner/WorldEditorInner.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { ChangeEventHandler, useMemo, useState } from "react";
 import styled from "styled-components";
 
 import { ReactComponent as PenTool } from '../../atoms/PenTool.svg';
@@ -57,6 +57,101 @@ const Container = styled.div`
     scrollbar-width: thin; // for FF
 `;
 
+const IframeInputWrapper = styled.div`
+    width: 428px;
+
+    margin: 18px;
+
+    display: flex;
+
+    flex-direction: column;
+    align-items: center;
+
+    border-radius: 23px;
+
+    background: #A69B97;
+`;
+
+const IframeTitle = styled.span`
+    margin: 16px;
+
+    font-family: 'Noto Sans';
+    font-size: 16px;
+    font-weight: 500;
+`;
+
+const ListFakeHr = styled.div`
+    width: 100%;
+    height: 2px;
+    background-color: #FFFFFF60;
+`;
+
+const IframeInput = styled.input`
+    width: calc(100% - 14px);
+    height: 42px;
+
+    box-sizing: border-box;
+
+    margin: 7px;
+    padding-left: 10px;
+
+    border-radius: 38px;
+    border: 0px;
+
+    background: #FFFFFB;
+    box-shadow: 5px 5px 20px rgba(0, 0, 0, 0.12);
+`;
+
+const IframeInputSettingWrapper = styled.div`
+    width: 100%;
+    height: 48px;
+
+    display: flex;
+`;
+
+const IframeInputSettingLeft = styled.div`
+    width: calc(50% - 1px);
+    height: 100%;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const VerticalLine = styled.div`
+    width: 0px;
+    height: 48px;
+
+    border: 1px solid rgba(255, 255, 255, 0.6);
+`;
+
+const IframeInputSettingRight = styled.div`
+    width: calc(50% - 1px);
+    height: 100%;
+
+    display: flex;
+
+    flex-direction: center;
+    justify-content: center;
+    align-items: center;
+`;
+
+const IframeInputSettingRightText = styled.span`
+    margin: 16px 18px;
+
+    font-family: Noto Sans;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 12px;
+    line-height: 16px;
+`;
+
+const IframeInputSettingRightVerticalLine = styled.div`
+    width: 0px;
+    height: 24px;
+
+    border: 1px solid rgba(255, 255, 255, 0.6);
+`
 
 const ToolsWrapper = styled.div`
     width: 100%;
@@ -97,10 +192,39 @@ function WorldEditorInner({ worldId, opened }: PropsType) {
         right: PhotoElementData[];
     }>({left: [], right: []});
 
+    const [iframeWidth, setIframeWidth] = useState(1);
+    const [iframeHeight, setIframeHeight] = useState(1);
+
     return (
         <ExpandBarDiv opened={opened}>
             <Container>
                 <DualTabList datas={datas} setId={setPhotoId} id={photoId} tab={tab} setTab={setTab} tabNames={tabNames}/>
+                <IframeInputWrapper>
+                    <IframeTitle>iframe address</IframeTitle>
+                    <ListFakeHr />
+                    <IframeInput type="text" placeholder="Add iframe address" />
+                    <ListFakeHr />
+                    <IframeInputSettingWrapper>
+                        <IframeInputSettingLeft>
+                            <IframeSettingLeftInput label="W" value={iframeWidth} onChange={e => setIframeWidth(+e.target.value)} />
+                            <IframeSettingLeftInput label="H" value={iframeHeight} onChange={e => setIframeHeight(+e.target.value)} />
+                        </IframeInputSettingLeft>
+                        <VerticalLine />
+                        <IframeInputSettingRight>
+                            <IframeInputSettingRightText>
+                                Wall
+                            </IframeInputSettingRightText>
+                            <IframeInputSettingRightVerticalLine />
+                            <IframeInputSettingRightText>
+                                floor
+                            </IframeInputSettingRightText>
+                            <IframeInputSettingRightVerticalLine />
+                            <IframeInputSettingRightText>
+                                effect
+                            </IframeInputSettingRightText>
+                        </IframeInputSettingRight>
+                    </IframeInputSettingWrapper>
+                </IframeInputWrapper>
             </Container>
             <ToolsWrapper>
                 <PenTool />
@@ -115,3 +239,67 @@ function WorldEditorInner({ worldId, opened }: PropsType) {
 
 
 export default React.memo(WorldEditorInner);
+
+
+const IframeSettingLeftInputWrapper = styled.div`
+    width: 70px;
+    height: 27px;
+
+    display: flex;
+
+    margin: 10px;
+
+    border-radius: 13.5px;
+`;
+
+const IframeSettingLeftInputLabel = styled.span`
+    width: 30px;
+    height: 27px;
+    line-height: 27px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    font-family: Noto Sans;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 12px;
+    line-height: 16px;
+
+    background-color: #FFFFFB;
+
+    border-radius: 13.5px 0px 0px 13.5px;
+`;
+
+const IframeSettingLeftInputArea = styled.input`
+    width: calc(100% - 30px);
+    height: 27px;
+    line-height: 27px;
+
+    box-sizing: border-box;
+
+    border: none;
+    outline: none;
+
+    background-color: #FFFFFB;
+
+    border-radius: 0px 13.5px 13.5px 0px;
+`;
+
+interface IframeSettingLeftInputProps {
+    label: string;
+    value: number;
+    onChange: ChangeEventHandler<HTMLInputElement>
+}
+
+function IframeSettingLeftInput({label, value, onChange}: IframeSettingLeftInputProps) {
+    return (
+        <IframeSettingLeftInputWrapper>
+            <IframeSettingLeftInputLabel>
+                {label} :
+            </IframeSettingLeftInputLabel>
+            <IframeSettingLeftInputArea onChange={onChange} value={value} />
+        </IframeSettingLeftInputWrapper>
+    )
+}
