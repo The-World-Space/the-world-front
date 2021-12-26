1e521299a8d7c913299d910eff0c5188bd2da380
SungU Kang <20sunrin129@sunrint.hs.kr>
2021-12-26 13:11:35 +0900

    ImageNetworker

---
+++ src/game/script/networker/ImageNetworker.ts
@@ -0,0 +1,103 @@
+import { ApolloClient, gql } from "@apollo/client";
+import { DumbTypedEmitter } from "detail-typed-emitter";
+import { Server } from "../../connect/types";
+
+const IMAGE_FILEDS = gql`
+fragment ImageFields on ImageGameObject {
+    id
+    x
+    y
+    proto {
+        id
+        name
+        owner {
+            id
+            nickname
+            skinSrc
+        }
+        isPublic
+        width
+        height
+        offsetX
+        offsetY
+        type
+        colliders {
+            x
+            y
+            isBlocked
+        }
+        src
+    }
+}
+`
+
+
+type imageId = number;
+
+
+type DEETypes = {
+    "create" : (iframeInfo: Server.ImageGameObject) => void,
+    "delete" : (id: imageId) => void
+}
+
+
+export class ImageNetworker {
+    private readonly _dee: DumbTypedEmitter<DEETypes>;
+
+    constructor(private readonly _worldId: string,
+                private readonly _client: ApolloClient<any>) {
+        this._dee = new DumbTypedEmitter<DEETypes>();
+        this._initNetwork();
+        this._initEEListenters();
+    }
+
+    private _initNetwork() {
+        this._client.subscribe({
+            query: gql`
+                subscription ImageGOCreateing($worldId: String!) {
+                    imageGameObjectCreating(worldId: $worldId) {
+                        ...ImageFields
+                    }
+                }
+
+                ${IMAGE_FILEDS}
+            `,
+            variables: {
+                worldId: this._worldId,
+            }
+        }).subscribe(data => {
+            if (!data.data.imageGameObjectCreating) throw new Error("data.data.iframeGameObjectCreating is falsy");
+            const iframeInfo = data.data.imageGameObjectCreating as Server.ImageGameObject;
+            
+            this._dee.emit("create", iframeInfo);
+        });
+
+        this._client.subscribe({
+            query: gql`
+                subscription ImageGODeleting($worldId: String!) {
+                    imageGameObjectDeleting(worldId: $worldId)
+                }
+            `,
+            variables: {
+                worldId: this._worldId,
+            }
+        }).subscribe(data => {
+            if (!data.data.imageGameObjectDeleting) throw new Error("data.data.iframeGameObjectDeleting is falsy");
+            const deletingId = data.data.imageGameObjectDeleting as imageId;
+            
+            this._dee.emit("delete", deletingId);
+        });
+    }
+
+
+    private _initEEListenters() {
+        
+        
+    }
+
+
+    get ee(): DumbTypedEmitter<DEETypes> {
+        return this._dee;
+    }
+}
+
