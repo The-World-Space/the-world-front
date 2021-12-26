999be8d0bb5c7ed96d02a9f43e0bf2389aa589e7
noname <48761044+noname0310@users.noreply.github.com>
2021-12-26 22:59:00 +0900

    user info page wip

---
+++ src/pages/UserInfo.tsx
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+import NavTemplate from "../components/templates/NavTemplate";
+
+const ContentDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    box-sizing: border-box;
+    flex: 1;
+`;
+
+const ContentDetailDiv = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+    width: 100%;
+`;
+
+function UserInfo(): JSX.Element {
+    return (
+        <NavTemplate showNavContent={true}>
+            <ContentDiv>
+                <div>
+                    {/* div horizontal line */}
+                    <div style={{ borderBottom: "1px solid #000000C0", width: "100%", margin: "10px 0px" }} />
+                </div>
+                <ContentDetailDiv style={{
+                    marginTop: "86px",
+                }}>
+                    <div style={{ textAlign: "right" }}>
+                        사용자 지정 기능 확장이 가능한 2D 메타버스 서비스입니다.<br/> 탑다운 뷰로, 사용자는 원하는 사용자 지정 서비스를 바닥이나 건물로서 배치할 수 있습니다.<br/> 사용자 지정 서비스를 만드는 경우, 바닥이나 건물과의 상호작용을 원하는 대로 처리할 수 있습니다.
+                    </div>
+                    <div style={{
+                        borderLeft: "1px solid #000000C0",
+                        height: "110px",
+                        margin: "0% 2% 0% 2%",
+                    }}/>
+                    <img src={`${process.env.PUBLIC_URL}/assets/welcome_img.png`} alt={"welcome img"}
+                        style={{
+                            height: "300px",
+                            margin: "0% 2% 0% 2%",
+                            borderRadius: "35px",
+                        }}/>
+                </ContentDetailDiv>
+            </ContentDiv>
+        </NavTemplate>
+    );
+}
+
+export default UserInfo;
