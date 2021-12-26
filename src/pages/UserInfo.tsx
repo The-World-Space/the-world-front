364f4889370aac05d45b92f65e4a84f7e0ed4719
noname <48761044+noname0310@users.noreply.github.com>
2021-12-26 23:52:33 +0900

    Update UserInfo.tsx

--- src/pages/UserInfo.tsx
+++ src/pages/UserInfo.tsx
@@ -1,51 +1,43 @@
 import styled from "styled-components";
 import NavTemplate from "../components/templates/NavTemplate";
+import { FORM_FONT_FAMILY } from "./GlobalEnviroment";
 
 const ContentDiv = styled.div`
     display: flex;
     flex-direction: column;
-    justify-content: center;
+    justify-content: flex-start;
     align-items: center;
     width: 100%;
     box-sizing: border-box;
     flex: 1;
-`;
-
-const ContentDetailDiv = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-    flex-wrap: wrap;
-    width: 100%;
+    margin-top: 60px;
 `;
 
 function UserInfo(): JSX.Element {
     return (
         <NavTemplate showNavContent={true}>
             <ContentDiv>
-                <div>
-                    {/* div horizontal line */}
-                    <div style={{ borderBottom: "1px solid #000000C0", width: "100%", margin: "10px 0px" }} />
+                <div style={{ width: "80%", fontSize: "32px", fontFamily: FORM_FONT_FAMILY }}>
+                    User Infomation
                 </div>
-                <ContentDetailDiv style={{
-                    marginTop: "86px",
+                <div style={{ borderBottom: "2px solid #000000C0", width: "100%", margin: "20px 0px" }} />
+                <div style={{
+                    height: "350px",
+                    width: "80%",
+                    margin: "0% 2% 0% 2%",
+                    borderRadius: "35px",
+                    backgroundColor: "#FFFFFF",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
                 }}>
-                    <div style={{ textAlign: "right" }}>
-                        사용자 지정 기능 확장이 가능한 2D 메타버스 서비스입니다.<br/> 탑다운 뷰로, 사용자는 원하는 사용자 지정 서비스를 바닥이나 건물로서 배치할 수 있습니다.<br/> 사용자 지정 서비스를 만드는 경우, 바닥이나 건물과의 상호작용을 원하는 대로 처리할 수 있습니다.
-                    </div>
                     <div style={{
                         borderLeft: "1px solid #000000C0",
-                        height: "110px",
+                        height: "100%",
                         margin: "0% 2% 0% 2%",
                     }}/>
-                    <img src={`${process.env.PUBLIC_URL}/assets/welcome_img.png`} alt={"welcome img"}
-                        style={{
-                            height: "300px",
-                            margin: "0% 2% 0% 2%",
-                            borderRadius: "35px",
-                        }}/>
-                </ContentDetailDiv>
+                </div>
             </ContentDiv>
         </NavTemplate>
     );
