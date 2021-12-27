97757b351b91b957cebe35eeb0243c5976d3eaab
ingbunga <20sunrin129@sunrint.hs.kr>
2021-12-27 10:55:03 +0900

    tile networker

--- src/game/script/networker/TileNetworker.ts
+++ src/game/script/networker/TileNetworker.ts
@@ -1,44 +1,73 @@
-// import { ApolloClient, gql } from "@apollo/client";
-// import { DumbTypedEmitter } from "detail-typed-emitter";
-// import { Server } from "../../connect/types";
+import { ApolloClient, gql } from "@apollo/client";
+import { DumbTypedEmitter } from "detail-typed-emitter";
+import { Server } from "../../connect/types";
 
 
-// type DEETypes = {
-//     "update" : (collider: Server.Collider) => void,
-// }
+type DEETypes = {
+    "create" : (collider: Server.AtlasTile) => void,
+    "delete" : (x: number, y: number, type: number) => void,
+}
 
 
 export class TileNetworker {
-    // private readonly _dee: DumbTypedEmitter<DEETypes>;
+    private readonly _dee: DumbTypedEmitter<DEETypes>;
 
-    // constructor(private readonly _worldId: string,
-    //             private readonly _client: ApolloClient<any>) {
-    //     this._dee = new DumbTypedEmitter<DEETypes>();
-    //     this._initNetwork();
-    //     // this._initEEListenters();
-    // }
+    constructor(private readonly _worldId: string,
+                private readonly _client: ApolloClient<any>) {
+        this._dee = new DumbTypedEmitter<DEETypes>();
+        this._initNetwork();
+        // this._initEEListenters();
+    }
 
-    // private _initNetwork() {
-    //     this._client.subscribe({
-    //         query: gql`
-    //             subscription ColliderUpdating($worldId: String!) {
-    //                 colliderUpdating(worldId: $worldId) {
-    //                     x
-    //                     y
-    //                     isBlocked
-    //                 }
-    //             }
-    //         `,
-    //         variables: {
-    //             worldId: this._worldId,
-    //         }
-    //     }).subscribe(data => {
-    //         if (!data.data.colliderUpdating) throw new Error("data.data.iframeGameObjectCreating is falsy");
-    //         const collider = data.data.colliderUpdating as Server.Collider;
+    private _initNetwork() {
+        this._client.subscribe({
+            query: gql`
+                subscription atlasTileCreating($worldId: String!) {
+                    atlasTileCreating(worldId: $worldId) {
+                        x
+                        y
+                        type
+                        atlasIndex
+                        atlas {
+                            id
+                            name
+                            columnCount
+                            rowCount
+                            src
+                        }
+                    }
+                }
+            `,
+            variables: {
+                worldId: this._worldId,
+            }
+        }).subscribe(data => {
+            if (!data.data.atlasTileCreating) throw new Error("data.data.atlasTileCreating is falsy");
+            const tile = data.data.atlasTileCreating as Server.AtlasTile;
             
-    //         this._dee.emit("update", collider);
-    //     });
-    // }
+            this._dee.emit("create", tile);
+        });
+
+        this._client.subscribe({
+            query: gql`
+                subscription atlasTileDeleting($worldId: String!) {
+                    atlasTileDeleting(worldId: $worldId) {
+                        x
+                        y
+                        type
+                    }
+                }
+            `,
+            variables: {
+                worldId: this._worldId,
+            }
+        }).subscribe(data => {
+            if (!data.data.atlasTileDeleting) throw new Error("data.data.atlasTileDeleting is falsy");
+            const tile = data.data.atlasTileDeleting as Server.AtlasTile;
+
+            this._dee.emit("delete", tile.x, tile.y, tile.type);
+        });
+    }
 
 
     // private _initEEListenters() {
@@ -47,8 +76,8 @@ export class TileNetworker {
     // }
 
 
-    // get ee(): DumbTypedEmitter<DEETypes> {
-    //     return this._dee;
-    // }
+    get ee(): DumbTypedEmitter<DEETypes> {
+        return this._dee;
+    }
 }
 
