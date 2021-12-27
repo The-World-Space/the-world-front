88c237d9c3fcba662baa58cd77a172321b28d83c
SungU Kang <20sunrin129@sunrint.hs.kr>
2021-12-28 02:40:07 +0900

    logout cache clear

--- src/pages/Logout.tsx
+++ src/pages/Logout.tsx
@@ -4,14 +4,19 @@ import {
 } from "react-router-dom";
 import { AuthContext } from "../context/contexts";
 import { useContext } from "react";
+import { useApolloClient } from "@apollo/client";
 
 function Logout(): JSX.Element {
+    const apolloClient = useApolloClient();
     const history = useHistory();
     const { setJwt } = useContext(AuthContext);
 
     useEffect(() => {
         setJwt("");
-        history.push("/");
+        apolloClient.resetStore()
+            .then(() => {
+                history.push("/");
+            });
     });
 
     return (
