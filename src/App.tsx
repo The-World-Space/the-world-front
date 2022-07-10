5977e1ae4b56c27f341c8bd665012a0067cbc35c
noname <48761044+noname0310@users.noreply.github.com>
2022-07-10 14:21:02 +0900

    change lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import Welcome from "./pages/Welcome";
+import Welcome from './pages/Welcome';
 
 function App(): JSX.Element {
     return (
