c30d62eea3991060eb936e038ad3b2ab4d26b3a1
noname <48761044+noname0310@users.noreply.github.com>
2022-07-10 14:40:59 +0900

    add not found

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Welcome from './pages/Welcome';
+import NotFound from './pages/NotFound';
 
 function App(): JSX.Element {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<Welcome />} />
+                <Route path='/welcome' element={<Welcome />} />
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </BrowserRouter>
     );
