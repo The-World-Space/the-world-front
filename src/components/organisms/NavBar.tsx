7d050168620c28844ead73a41ee508977c8c29f5
noname <48761044+noname0310@users.noreply.github.com>
2022-07-11 22:22:24 +0900

    Update NavBar.tsx

--- src/components/organisms/NavBar.tsx
+++ src/components/organisms/NavBar.tsx
@@ -1,4 +1,7 @@
-import { Link } from 'react-router-dom';
+import { 
+    Link,
+    useNavigate
+} from 'react-router-dom';
 import styled from 'styled-components';
 import {
     useState,
@@ -7,6 +10,7 @@ import {
 } from 'react';
 import useDebounce from '../../hooks/useDebounce';
 import MenuButton from '../atoms/MenuButton';
+import { PaddingDiv } from '../atoms/styled';
 
 const NavBarOuterDiv = styled.div`
     display: flex;
@@ -17,6 +21,9 @@ const NavBarOuterDiv = styled.div`
     width: 100%;
     background-color: #272727;
     box-sizing: border-box;
+
+    position: relative;
+    z-index: 1;
 `;
 
 const NavBarDiv = styled.div`
@@ -70,8 +77,14 @@ const UserInfoDiv = styled.div`
 `;
 
 function PcNavBar(): JSX.Element {
+    const navigate = useNavigate();
+
+    const handleLoginClick = useCallback(() => {
+        navigate('/login');
+    }, [navigate]);
+
     return (
-        <>
+        <NavBarOuterDiv>
             <NavBarLeftDiv>
                 <Link to={'/'}>
                     <NavBarLogo/>
@@ -88,14 +101,44 @@ function PcNavBar(): JSX.Element {
             </NavBarLeftDiv>
             <NavBarRightDiv>
                 <UserInfoDiv>
-                    <NavBarButton>
+                    <NavBarButton onClick={handleLoginClick}>
                         Sign In
                     </NavBarButton>
                 </UserInfoDiv>
             </NavBarRightDiv>
-        </>
+        </NavBarOuterDiv>
     );
 }
+
+interface MobileNavBarPanelDivProps {
+    isOpen: boolean;
+}
+
+const MobileNavBarPanelDiv = styled.div<MobileNavBarPanelDivProps>`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    position: absolute;
+    top: 60px;
+    width: 100%;
+
+    background-color: #272727;
+    transition: 0.3s;
+    transform: ${props => props.isOpen ? 'translateY(0%)' : 'translateY(-100%)'};
+`;
+
+const NavBarMobileButton = styled(NavBarButton)`
+    height: 50px;
+    width: 100%;
+
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+
+    padding: 0px 40px;
+`;
 
 function MobileNavBar(): JSX.Element {
     const [isOpen, setIsOpen] = useState(false);
@@ -106,12 +149,29 @@ function MobileNavBar(): JSX.Element {
 
     return (
         <>
-            <NavBarLeftDiv>
-                <NavBarLogo />
-            </NavBarLeftDiv>
-            <NavBarRightDiv>
-                <MenuButton isOpen={isOpen} onClick={handleClick}/>
-            </NavBarRightDiv>
+            <NavBarOuterDiv>
+                <NavBarLeftDiv>
+                    <NavBarLogo />
+                </NavBarLeftDiv>
+                <NavBarRightDiv>
+                    <MenuButton isOpen={isOpen} onClick={handleClick}/>
+                </NavBarRightDiv>
+            </NavBarOuterDiv>
+            <MobileNavBarPanelDiv isOpen={isOpen}>
+                <NavBarMobileButton>
+                    Button1
+                </NavBarMobileButton>
+                <NavBarMobileButton>
+                    Button2
+                </NavBarMobileButton>
+                <NavBarMobileButton>
+                    Button3
+                </NavBarMobileButton>
+                <PaddingDiv height="30px"/>
+                <NavBarMobileButton>
+                    Sign In
+                </NavBarMobileButton>
+            </MobileNavBarPanelDiv>
         </>
     );
 }
@@ -139,9 +199,9 @@ function NavBar(): JSX.Element {
     }, [handleResize]);
 
     return (
-        <NavBarOuterDiv>
+        <>
             {!debouncedIsMobile ? <PcNavBar/> : <MobileNavBar/>}
-        </NavBarOuterDiv>
+        </>
     );
 }
 
