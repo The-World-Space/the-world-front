57a42375ce7d42cc8ae6526851e6082b057e5a38
noname <48761044+noname0310@users.noreply.github.com>
2022-08-31 22:16:31 +0900

    Update MovementAnimationController.ts

--- src/game/script/controller/MovementAnimationController.ts
+++ src/game/script/controller/MovementAnimationController.ts
@@ -9,7 +9,7 @@ export class MovementAnimationController extends Component {
     private _lastDirection: Direction = Direction.Down;
     private _lastIsMoving = false;
 
-    protected awake(): void {
+    public awake(): void {
         this._directable = this.gameObject.getComponent(Directable);
         this._spriteAtlasAnimator = this.gameObject.getComponent(SpriteAtlasAnimator);
     }
@@ -17,58 +17,46 @@ export class MovementAnimationController extends Component {
     public update(): void {
         const direction = this._directable!.direction;
         const isMoving = this._directable!.isMoving;
+        
         if (isMoving) {
             if (direction === Direction.Up) {
                 if (this._lastIsMoving !== isMoving || this._lastDirection !== direction) {
                     this._spriteAtlasAnimator!.playAnimation("up_walk");
-                    this._lastIsMoving = isMoving;
-                    this._lastDirection = direction;
                 }
             } else if (direction === Direction.Down) {
                 if (this._lastIsMoving !== isMoving || this._lastDirection !== direction) {
                     this._spriteAtlasAnimator!.playAnimation("down_walk");
-                    this._lastIsMoving = isMoving;
-                    this._lastDirection = direction;
                 }
             } else if (direction === Direction.Left) {
                 if (this._lastIsMoving !== isMoving || this._lastDirection !== direction) {
                     this._spriteAtlasAnimator!.playAnimation("left_walk");
-                    this._lastIsMoving = isMoving;
-                    this._lastDirection = direction;
                 }
             } else if (direction === Direction.Right) {
                 if (this._lastIsMoving !== isMoving || this._lastDirection !== direction) {
                     this._spriteAtlasAnimator!.playAnimation("right_walk");
-                    this._lastIsMoving = isMoving;
-                    this._lastDirection = direction;
                 }
             }
         } else {
             if (direction === Direction.Up) {
                 if (this._lastIsMoving !== isMoving || this._lastDirection !== direction) {
                     this._spriteAtlasAnimator!.playAnimation("up_idle");
-                    this._lastIsMoving = isMoving;
-                    this._lastDirection = direction;
                 }
             } else if (direction === Direction.Down) {
                 if (this._lastIsMoving !== isMoving || this._lastDirection !== direction) {
                     this._spriteAtlasAnimator!.playAnimation("down_idle");
-                    this._lastIsMoving = isMoving;
-                    this._lastDirection = direction;
                 }
             } else if (direction === Direction.Left) {
                 if (this._lastIsMoving !== isMoving || this._lastDirection !== direction) {
                     this._spriteAtlasAnimator!.playAnimation("left_idle");
-                    this._lastIsMoving = isMoving;
-                    this._lastDirection = direction;
                 }
             } else if (direction === Direction.Right) {
                 if (this._lastIsMoving !== isMoving || this._lastDirection !== direction) {
                     this._spriteAtlasAnimator!.playAnimation("right_idle");
-                    this._lastIsMoving = isMoving;
-                    this._lastDirection = direction;
                 }
             }
         }
+
+        this._lastIsMoving = isMoving;
+        this._lastDirection = direction;
     }
 }
